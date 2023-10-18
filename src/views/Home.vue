683d666ce7eb81438b019a21ae2e775eee086b33
Edward Tsai <fashion.donk@gmail.com>
2023-10-18 10:49:51 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/all.scss";
+// @import "@/assets/scss/page/about.scss";
+</style>
