d5f6e3cd368b423e238778d03b59ffe00b91da91
張嘉哲 <polikuj1@yahoo.com.tw>
2023-11-04 19:57:09 +0800

    feat: side menu

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -2,26 +2,30 @@
   <div class="side-menu">
     <div class="top-navgation">
       <div class="selection">
-        <select name="" id="" class="city">
-          <option value="" default>178798</option>
-          <option value="">12</option>
-          <option value="">12</option>
-        </select>
-        <select name="" id="" class="area">
-          <option value="" default>12131</option>
-          <option value="">12</option>
-          <option value="">12</option>
-        </select>
+        <img src="../../public/arrow-active.svg" alt="" class="city-arrow" @click="openCitySelection">
+        <img src="../../public/arrow.svg" alt="" class="area-arrow">
+        <div class="city" @click="openCitySelection" :class="{on: citySelectionOpen}">
+          <span v-if="!currentCity">選擇縣市</span>
+          <span v-else>{{ currentCity }}</span>
+          <ul class="option" v-show="citySelectionOpen">
+            <li v-for="city in cities" :key="city" @click="selectCity(city)">{{ city }}</li>
+          </ul>
+        </div>
+        <div class="area">12131</div>
       </div>
       <div class="candidate-all">
         <div class="candidate">
-          <img src="https://picsum.photos/id/684/600/400" alt="">
+          <img src="https://picsum.photos/id/684/100/100" alt="">
           <span>gordon</span>
           <span>35%</span>
         </div>
       </div>
     </div>
     <div class="each-area-ratio">
+      <div class="title">
+        <span>縣市</span>
+        <span>得票占比</span>
+      </div>
       <div v-for="data in datas" :key="data.name">
           <VoteBar :data="data"/>
       </div>
@@ -29,31 +33,53 @@
   </div>
 </template>
 <script>
-const datas = [
-  {
-    name: "taipei",
-    vote: {
-      "民進黨": 1000000,
-      "國民黨": 54515,
-      "民眾黨": 850000,
-    },
-  },
-  {
-    name: "taichung",
-    vote: {
-      "民進黨": 200000,
-      "國民黨": 60000,
-      "民眾黨": 450000,
-    },
-  },
-]
 import { ref, reactive } from "vue";
-import VoteBar from "@/components/VoteBar";
+import VoteBar from "./VoteBar.vue";
 export default {
   name: "SideMenu",
-  components: [VoteBar],
+  components: { VoteBar },
   setup() {
+    const cities = ref(["台北市", "新北市", "桃園市", "台中市", "台南市", "高雄市", "基隆市", "新竹市", "新竹縣", "新北市", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義市", "嘉義縣", "屏東縣", "台東縣", "花蓮縣", "宜蘭縣", "金門縣", "連江縣"]);
+    const citySelectionOpen = ref(false);
+    const currentCity = ref("");
+    const openCitySelection = () => {
+      citySelectionOpen.value = !citySelectionOpen.value;
+    }
+    const selectCity = (city) => {
+      currentCity.value = city;
+    }
+    
+    
+    const datas = reactive([
+      {
+        name: "taipei",
+        vote: {
+          "民進黨": 1000000,
+          "國民黨": 54515,
+          "親民黨": 850000,
+        },
+      },
+      {
+        name: "taichung",
+        vote: {
+          "民進黨": 200000,
+          "國民黨": 60000,
+          "親民黨": 450000,
+        },
+      },
+    ]);
+    
 
+
+
+    return { 
+      datas, 
+      cities,
+      citySelectionOpen,
+      openCitySelection,
+      selectCity,
+      currentCity,
+    };
   },
 }
 
--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -1,33 +1,28 @@
 <script setup>
-import { ref, reactive, onMounted } from 'vue'
-import * as d3 from 'd3'
+import { ref, onMounted, nextTick } from "vue";
+import * as d3 from "d3";
+import * as topojson from "topojson-client";
 import VoteBar from "@/components/VoteBar.vue";
 import SideMenu from "@/components/SideMenu.vue";
-const datas = [
-  {
-    name: "taipei",
-    vote: {
-      "民進黨": 1000000,
-      "國民黨": 54515,
-      "民眾黨": 850000,
-    },
-  },
-  {
-    name: "taichung",
-    vote: {
-      "民進黨": 200000,
-      "國民黨": 60000,
-      "民眾黨": 450000,
-    },
-  },
-]
-const count = ref(0)
-// d3.csv.parse('./public/2020_presedent/總統-A05-1-候選人得票數一覽表(中　央).csv').then((data)=>{
-//   console.log(data);
-// })
-//畫台灣地圖
-function drawMap(el, data) {
-  const d3map = d3.select(el)
+
+const taiwanMapData = ref([]);
+const cityMapData = ref([]);
+const districtMapData = ref([]);
+
+//抓台灣地圖資料
+function getMap(level = "nation", zone = "nation") {
+    let file = "";
+    if (level === "nation") {
+        file = "nation/COUNTY_MOI_1090820.json";
+    } else if (level === "city") {
+        file = "city/TOWN_MOI_1120825.json";
+    }
+
+    d3.json(`public/tw_map/${file}`).then((data) => {
+        drawMap("#nation-map", data);
+    });
+
+    //畫台灣地圖
     let scale = 5500;
     let wWidth = window.innerWidth;
     if (wWidth >= 1280) {
@@ -39,117 +34,172 @@ function drawMap(el, data) {
         scale = 5500;
     }
 
-    const country = topojson.feature(data, data.objects.COUNTY_MOI_1090820);
+    // let mapWidth = 500
+    // let mapHeight = 650
     let projection = d3.geoMercator().center([122.3, 24]).scale(scale);
     let path = d3.geoPath();
 
-    const counties = d3map
-        .selectAll("path")
-        .data(country.features)
-        .join("g")
-        .attr("id", (d) => d.properties.COUNTYNAME)
-        .classed("county", true);
+    function drawMap(el, data) {
+        const d3map = d3.select(el);
+        let mapData;
+        let mapSelectClass;
+        let choosedZoneMapData = [];
+        if (level === "nation") {
+            mapData = topojson.feature(data, data.objects.COUNTY_MOI_1090820);
+            taiwanMapData.value = mapData.features;
+            mapSelectClass = `.county`;
+            choosedZoneMapData = mapData.features;
+            console.log(choosedZoneMapData);
+        } else if (level === "city") {
+            mapData = topojson.feature(data, data.objects.TOWN_MOI_1120825);
+            choosedZoneMapData = mapData.features.filter(
+                (district) => district.properties.COUNTYNAME === zone
+            );
+            cityMapData.value = choosedZoneMapData;
+            mapSelectClass = `.district [data-city=${zone}]`;
+        }
 
-    counties.append("title").text((d) => d.properties.COUNTYNAME);
-    counties.append("path").attr("d", path.projection(projection));
-    //事件綁定
-    counties.on("click", mapZoom);
+        nextTick(() => {
 
-    //點擊縮放區域
-    const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
+            d3map
+                .selectAll(mapSelectClass)
+                .data(choosedZoneMapData)
+                .join("g")
+                .append("path")
+                .attr("d", path.projection(projection))
+                .on("click", mapZoom);
+        });
+        //事件綁定
+        // counties;
 
-    function zoomed(event) {
-        const { transform } = event;
-        console.log(transform);
-        d3map.attr("transform", transform);
-        d3map.attr("stroke-width", 1 / transform.k);
-    }
+        //點擊縮放區域
+        const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
 
-    function mapZoom(event, d) {
-        const coutyElement = document.getElementById(d.properties.COUNTYNAME)
-        const [[x0, y0], [x1, y1]] = path.bounds(d);
-        const width = 500;
-        const height = 650;
-        // const scale = Math.min(width/(x1 - x0), height/(y1 - y0))*0.8
-        const scale = 2
-        console.log('x0:', x0);
-        console.log('y0:', y0);
-        // event.stopPropagation();
-        d3.select(this)
-            .transition()
-            .duration(750)
-            .call(
+        function zoomed(event) {
+            const { transform } = event;
+            d3map.attr("transform", transform);
+            d3map.attr("stroke-width", 1 / transform.k);
+        }
+
+        function mapZoom(event, d) {
+            const [[x0, y0], [x1, y1]] = path.bounds(d);
+
+            // const scale = Math.min(width/(x1 - x0), height/(y1 - y0))*0.8
+            const scale = 1;
+            // event.stopPropagation();
+            d3.select(this).transition().duration(750).call(
                 zoom.transform,
                 d3.zoomIdentity
-                // .translate(width / 2, height / 2)
-                .scale(scale),
+                    // .translate(mapWidth / 2, mapHeight / 2)
+                    .scale(scale),
                 d3.pointer(event, d3map.node())
             );
-    }
+        }
 
-    d3map.call(zoom);
+        d3map.call(zoom);
+    }
+}
+
+//點擊區域顯示投票結果
+function showDistrictOrVilla(ZoneProperties) {
+    if(ZoneProperties.COUNTYNAME && !ZoneProperties.TOWNNAME){
+        getMap("city", ZoneProperties.COUNTYNAME);
+    }
 }
 
 onMounted(() => {
-    d3.json("public/tw_map/nation/COUNTY_MOI_1090820.json").then((data) => {
-        drawMap(".map svg", data);
-    });
+    getMap("nation");
 });
-
 </script>
 
 <template>
-  <h2>歷史開票</h2>
-  <div class="map"></div>
-  <SideMenu />
-  <div v-for="data in datas" :key="data.name">
-          <VoteBar :data="data"/>
-  </div>
+    <div class="vote-page">
+      <h2>歷史開票</h2>
+      <SideMenu />
+      <div class="map">
+          <svg ref="mapRef">
+              <g id="nation-map">
+                  <g
+                      class="county"
+                      v-for="city in taiwanMapData"
+                      :id="city.properties.COUNTYNAME"
+                      :key="city.properties.COUNTYCODE"
+                      @click="showDistrictOrVilla(city.properties)"
+                  >
+                      <title>{{ city.properties.COUNTYNAME }}</title>
+                  </g>
+                  <g class="district">
+                      <g
+                          v-for="district in cityMapData"
+                          :key="district.properties.TOWNCODE"
+                          :id="district.properties.TOWNNAME"
+                          :data-city="district.properties.COUNTYNAME"
+                      >
+                          <title>{{ district.properties.TOWNNAME }}</title>
+                      </g>
+                  </g>
+                  <g class="village">
+                      <g></g>
+                  </g>
+              </g>
+          </svg>
+      </div>
+    </div>
 </template>
 
 <style lang="scss">
 @import "@/assets/scss/all.scss";
-.d-flex{
+.vote-page {
   display: flex;
-  justify-content: center;
-  align-items: center;
+  max-width: 1280px;
 }
-.map{
-  // overflow: hidden;
-  height: 650px;
-  width: 500px;
-  border: 1px solid #000;
-  position: relative;
-    svg {
-      display: block;
-      width: 100%;
-      height: 100%;
+.d-flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.map {
+    width: 70%;
+    // overflow: hidden;
+    height: 650px;
+    width: 500px;
+    border: 1px solid #000;
+    position: relative;
+    & > svg {
+        display: block;
+        width: 100%;
+        height: 100%;
     }
-  &::before{
-    content: '';
-    display: block;
-    width: 2px;
-    height: 2px;
-    background-color: red;
-    position: absolute;
-    bottom: 50%;
-    right: 50%;
-    z-index: 2;
-  }
+    #nation-map {
+        display: block;
+        height: 100%;
+        width: 100%;
+    }
+    &::before {
+        content: "";
+        display: block;
+        width: 2px;
+        height: 2px;
+        background-color: red;
+        position: absolute;
+        bottom: 50%;
+        right: 50%;
+        z-index: 2;
+    }
 }
 .county {
-    stroke: #000;
-    stroke-width: 0.2;
-    fill: rgb(90, 229, 175);
+    stroke: $bg;
+    stroke-width: 0.5;
+    fill: $green;
     &:hover {
-        fill: rgb(90, 166, 229);
+        fill: $blue-d;
         cursor: pointer;
     }
-    &.active {
-        fill: red;
-        &:hover {
-            fill: red;
-        }
-    }
+    
+}
+.district {
+    stroke: $bg;
+    stroke-width: 0.5;
+    fill: $blue-l;
 }
 </style>
